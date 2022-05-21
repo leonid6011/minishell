fe01210d8fee83308565aa499576d74454ac68a2
Eura Chrystal <echrysta@at-m7.msk.21-school.ru>
2022-05-21 16:06:24 +0300

    fix exho

--- echo.c
+++ echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbutter <mbutter@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: echrysta <echrysta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 14:54:32 by echrysta          #+#    #+#             */
-/*   Updated: 2022/05/14 18:06:30 by mbutter          ###   ########.fr       */
+/*   Updated: 2022/05/21 16:05:43 by echrysta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,78 +77,3 @@ int	echo(t_table_cmd *table)
 	}
 	return (EXIT_SUCCESS);
 }
-
-// static int	is_valid_option_n(char *str)
-// {
-// 	if (!*str || *str != '-')
-// 		return (0);
-// 	str++;
-// 	if (!*str || *str != 'n')
-// 		return (0);
-// 	while (*str)
-// 	{
-// 		if (*str != 'n')
-// 			return (0);
-// 		str++;
-// 	}
-// 	return (1);
-// }
-
-// void	skip_n_options(t_token *tokens, int *i, int *n_option)
-// {
-// 	while (tokens[*i].word && tokens[*i].type == WORD
-// 		&& is_valid_option_n(tokens[*i].word))
-// 	{
-// 		*n_option = 1;
-// 		(*i)++;
-// 	}
-// }
-
-
-// static void	skip_non_words(t_token *tokens, int *i)
-// {
-// 	while (tokens[*i].word && tokens[*i].type != WORD)
-// 		(*i)++;
-// }
-
-// static void	print_one_word(t_token *tokens, int *i)
-// {
-// 	ft_putstr_fd(tokens[*i].word, STDOUT_FILENO);
-// 	(*i)++;
-// }
-
-// static void	print_next_wds(t_token *tokens, int *i)
-// {
-// 	while (tokens[*i].word)
-// 	{
-// 		if (tokens[*i].type == WORD)
-// 		{
-// 			ft_putstr_fd(" ", STDOUT_FILENO);
-// 			ft_putstr_fd(tokens[*i].word, STDOUT_FILENO);
-// 		}
-// 		(*i)++;
-// 	}
-// }
-
-// int	builtin_echo(t_table_cmd *table)
-// {
-// 	int	i;
-// 	int	n_option;
-
-// 	// if (!proc)
-// 	// 	return ;
-// 	n_option = 0;
-// 	i = 0;
-// 	while (proc->tokens[i].word && proc->tokens[i].type != WORD)
-// 		i++;
-// 	if (proc->tokens[i].word)
-// 	{
-// 		skip_n_options(proc->tokens, &i, &n_option);
-// 		skip_non_words(proc->tokens, &i);
-// 		print_one_word(proc->tokens, &i);
-// 		print_next_wds(proc->tokens, &i);
-// 	}
-// 	if (!n_option)
-// 		ft_putstr_fd("\n", STDOUT_FILENO);
-// 	return (EXIT_SUCCESS);
-// }
